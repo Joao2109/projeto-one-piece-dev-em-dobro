f539d7e844e0c05b6f69e3d7fbe91d00ae61a7c0
Joao2109 <116182164+Joao2109@users.noreply.github.com>
2023-10-01 21:54:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+var personagem;
+var botao;
+var botao;
+var botao;
+function select(val) {
+    personagem = document.getElementsByClassName('personagem');
+    botao = document.getElementsByClassName('botao');
+    for (i = 0; i < botao.length; i++) {
+        personagem[i].classList.remove('selecionado');
+        botao[i].classList.remove('selecionado');
+    }
+    $(function () {
+        $(personagem).fadeOut(0);
+        $(personagem[val]).fadeIn(500);
+    });
+    botao[val].classList.add('selecionado');
+}
